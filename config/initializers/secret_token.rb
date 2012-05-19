326042ea9c85018ff4324c72b074744a72b9ba50
no <no@osmo.local>
2012-05-19 15:15:35 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsgirls::Application.config.secret_token = '8a681e5b2e1ee0cd9117e5cd67617a45967b714cec2c7d4b1df56db5e1f465db4370f0560d4ced094090d45a491ee6042b3245fcc944303e2dc65aa9434dc0e0'
